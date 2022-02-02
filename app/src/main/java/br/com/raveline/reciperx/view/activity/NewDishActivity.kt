d0513682316caf90b34daa87709364086f46c9df
ravelinejunior <junior.raveline@gmail.com>
2022-02-02 16:54:49 -0300

    New dish activity created

--- app/src/main/java/br/com/raveline/reciperx/view/activity/NewDishActivity.kt
+++ app/src/main/java/br/com/raveline/reciperx/view/activity/NewDishActivity.kt
@@ -2,11 +2,15 @@ package br.com.raveline.reciperx.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import br.com.raveline.reciperx.R
+import br.com.raveline.reciperx.databinding.ActivityNewDishBinding
 
 class NewDishActivity : AppCompatActivity() {
+    private lateinit var newDishBinding: ActivityNewDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_dish)
+        newDishBinding = DataBindingUtil.setContentView(this, R.layout.activity_new_dish)
+        setContentView(newDishBinding.root)
     }
 }
