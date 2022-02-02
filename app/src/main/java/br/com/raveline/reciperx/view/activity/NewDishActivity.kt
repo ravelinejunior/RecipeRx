51d014f64034014b1deabc444f058a87b9207be1
ravelinejunior <junior.raveline@gmail.com>
2022-02-02 15:45:32 -0300

    Refactoring

---
+++ app/src/main/java/br/com/raveline/reciperx/view/activity/NewDishActivity.kt
@@ -0,0 +1,12 @@
+package br.com.raveline.reciperx.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.raveline.reciperx.R
+
+class NewDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_dish)
+    }
+}
