6c59a293adcdb05653540c9b5bac97ae7a61479f
ravelinejunior <junior.raveline@gmail.com>
2022-02-04 15:19:22 -0300

    Permissions with Dexter Karumi

--- app/src/main/java/br/com/raveline/reciperx/view/activity/NewDishActivity.kt
+++ app/src/main/java/br/com/raveline/reciperx/view/activity/NewDishActivity.kt
@@ -1,15 +1,26 @@
 package br.com.raveline.reciperx.view.activity
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
 import br.com.raveline.reciperx.R
 import br.com.raveline.reciperx.databinding.ActivityNewDishBinding
 import br.com.raveline.reciperx.databinding.DialogCustomSelectImageBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
-class NewDishActivity : AppCompatActivity(),View.OnClickListener {
+class NewDishActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var newDishBinding: ActivityNewDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +32,7 @@ class NewDishActivity : AppCompatActivity(),View.OnClickListener {
         newDishBinding.imageViewNewDishNoImageId.setOnClickListener(this)
     }
 
-    private fun showCustomDialog(){
+    private fun showCustomDialog() {
         val dialog = Dialog(this)
         val dBinding = DialogCustomSelectImageBinding.inflate(layoutInflater)
         dialog.setContentView(dBinding.root)
@@ -31,12 +42,12 @@ class NewDishActivity : AppCompatActivity(),View.OnClickListener {
 
             //Camera
             textViewCustomDialogImageCameraId.setOnClickListener {
-                Toast.makeText(this@NewDishActivity, "Camera Text", Toast.LENGTH_SHORT).show()
+                getCameraPermissions()
                 dialog.dismiss()
             }
 
             imageViewCustomDialogImageCameraId.setOnClickListener {
-                Toast.makeText(this@NewDishActivity, "Camera Image", Toast.LENGTH_SHORT).show()
+                getCameraPermissions()
                 dialog.dismiss()
             }
 
@@ -54,7 +65,78 @@ class NewDishActivity : AppCompatActivity(),View.OnClickListener {
         }
     }
 
-    private fun setupActionBar(){
+    private fun getGalleryPermissions() {
+        Dexter.withContext(this@NewDishActivity).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report?.areAllPermissionsGranted() == true) {
+                    Toast.makeText(
+                        this@NewDishActivity,
+                        "Gallery permission granted!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    showRationalDialogForPermissions()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                requests: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun getCameraPermissions() {
+        Dexter.withContext(this@NewDishActivity).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report?.areAllPermissionsGranted() == true) {
+                    Toast.makeText(
+                        this@NewDishActivity,
+                        "Camera permission granted!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    showRationalDialogForPermissions()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                requests: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this@NewDishActivity)
+            .setMessage("To use this functionality you must grant all the required permissions.")
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun setupActionBar() {
         setSupportActionBar(newDishBinding.toolbarNewDishId)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         newDishBinding.apply {
@@ -65,9 +147,9 @@ class NewDishActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
+        when (v?.id) {
             R.id.imageViewNewDishAddNewImageId,
-            R.id.imageViewNewDishNoImageId ->{
+            R.id.imageViewNewDishNoImageId -> {
                 showCustomDialog()
             }
         }
